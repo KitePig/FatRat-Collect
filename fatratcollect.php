276a17c3abe3457c9375b8e9ddd0e689dcf01966
fubo <15210233861@163.com>
2018-12-04 16:09:26 +0800

    Create fatratcollect.php

---
+++ fatratcollect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: 胖鼠采集
+ * Plugin URI: #
+ * Description: 胖鼠采集系统
+ * Version: 0.0.1
+ * Author: Fat Rat
+ * Author URI: #
+ * Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+ * Text Domain: Fat Rat Collect
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
