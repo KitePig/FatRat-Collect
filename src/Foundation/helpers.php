82b548306ccfb4c2a0b65ff110784837666c3322
fubo <fubo@enle-inc.com>
2020-12-25 19:06:32 +0800

    .

--- src/Foundation/helpers.php
+++ src/Foundation/helpers.php
@@ -20,3 +20,37 @@ if (!function_exists("translationRules")) {
         return $array;
     }
 }
+
+if (!function_exists("frcAddColumn")) {
+    /**
+     * @param $column
+     * @param $afterColumn
+     * @param $table
+     */
+    function frcAddColumn($column, $afterColumn, $table): void
+    {
+        dd(getDb());
+        global $wpdb;
+        //Check for Exclude Image Path
+        $checkColumn = $wpdb->get_results($wpdb->prepare(
+            "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s AND COLUMN_NAME = %s ",
+            DB_NAME, $table, $column
+        )) ;
+        if ( empty( $checkColumn ) ) {
+            $alterSql = "ALTER TABLE `$table` ADD `{$column}` tinyint(2) NOT NULL DEFAULT 1 AFTER `{$afterColumn}`";
+            $wpdb->query($alterSql);
+        }
+    }
+}
+
+if (!function_exists("getDb")) {
+    /**
+     *
+     */
+    function getDb()
+    {
+        global $wpdb;
+
+        return $wpdb;
+    }
+}
